a68b85f49a991d865f229ae7c59527b8085c71ec
Moyuyanli <RoxyGreerad@163.com>
2024-04-03 17:12:09 +0800

    test:0.0.3

--- src/main/java/cn/chahuyun/session/HuYanSession.java
+++ src/main/java/cn/chahuyun/session/HuYanSession.java
@@ -19,7 +19,7 @@ import org.jetbrains.annotations.NotNull;
 public final class HuYanSession extends JavaPlugin {
 
     public static final HuYanSession INSTANCE = new HuYanSession();
-    public static final String VERSION = "0.0.2";
+    public static final String VERSION = "0.0.3";
     /**
      * 插件配置
      */
