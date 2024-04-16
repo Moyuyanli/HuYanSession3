17ba6b4eae97b9c19c5f0915c44d9156470a0a03
Moyuyanli <RoxyGreerad@163.com>
2024-04-16 11:30:47 +0800

    feat:添加群典功能开关

--- src/main/kotlin/cn/chahuyun/session/config/SessionPluginConfig.kt
+++ src/main/kotlin/cn/chahuyun/session/config/SessionPluginConfig.kt
@@ -31,6 +31,9 @@ object SessionPluginConfig : AutoSavePluginConfig("config") {
     @ValueDescription("是否默认开起本地缓存")
     var localCache: Boolean by value(false)
 
+    @ValueDescription("是否开启‘群典’快速添加多词条")
+    var groupCode: Boolean by value(false)
+
     @ValueDescription("是否匹配全部消息(true:响应所有能匹配成功的消息;false:只响应按照下列顺序匹配到的第一个)")
     var matchAll: Boolean by value(false)
 
